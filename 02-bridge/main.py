a7c983015d8473b898771e61af2945478994b12c
Daniel <d.preyl@web.de>
2024-06-17 18:39:09 +0200

    Ausbau für Meshtastic
    Aufnahme einer mestastic -> MQTT-Bridge mit Seriell angeschlossenem
    Device

--- 02-bridge/main.py
+++ 02-bridge/main.py
@@ -6,6 +6,7 @@ This script receives MQTT data and saves those to InfluxDB.
 
 """
 
+from flask import Flask, request, jsonify
 import re
 from typing import NamedTuple
 
@@ -13,8 +14,36 @@ import paho.mqtt.client as mqtt
 from influxdb import InfluxDBClient
 
 import time
+import copy
 
-INFLUXDB_ADDRESS = 'influxdb'
+import json
+
+from sqlalchemy import create_engine, Column, Integer, String, Text
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# SQLAlchemy Setup
+DATABASE_URI = 'sqlite:////nodedb/nodeinfoDB.db'
+engine = create_engine(DATABASE_URI)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+# NodeInfo Model
+class NodeInfo(Base):
+    __tablename__ = 'nodeinfo'
+    id = Column(Integer)
+    address = Column(Integer, primary_key=True, index=True)
+    hardware = Column(Integer)
+    longname = Column(String)
+    shortname = Column(String)
+
+# Initialize the database
+Base.metadata.create_all(bind=engine)
+
+# Initialize session
+session = SessionLocal()
+
+INFLUXDB_ADDRESS = 'influxdbZS'
 INFLUXDB_USER = 'root'
 INFLUXDB_PASSWORD = 'root'
 INFLUXDB_DATABASE = 'iothon_db'
@@ -22,17 +51,16 @@ INFLUXDB_DATABASE = 'iothon_db'
 MQTT_ADDRESS = 'mosquitto'
 MQTT_USER = 'mqttuser'
 MQTT_PASSWORD = 'mqttpassword'
-MQTT_TOPIC = 'iothon/+/+'  # [bme280|mijia]/[temperature|humidity|battery|status]
-MQTT_REGEX = 'iothon/([^/]+)/([^/]+)'
+MQTT_TOPIC = 'msh/ZS24/2/json/#'  # [bme280|mijia]/[temperature|humidity|battery|status]
 MQTT_CLIENT_ID = 'MQTTInfluxDBBridge'
+
+MQTT_TOPIC_NOTIFICATION = 'msh/ZS24/2/json/mqtt/'
 
 influxdb_client = InfluxDBClient(INFLUXDB_ADDRESS, 8086, INFLUXDB_USER, INFLUXDB_PASSWORD, None)
 
+nodeinfoDB = {}
 
-class SensorData(NamedTuple):
-    location: str
-    measurement: str
-    value: float
+app = Flask(__name__)
 
 
 def on_connect(client, userdata, flags, rc):
@@ -44,36 +72,88 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     """The callback for when a PUBLISH message is received from the server."""
     print(msg.topic + ' ' + str(msg.payload))
-    sensor_data = _parse_mqtt_message(msg.topic, msg.payload.decode('utf-8'))
-    if sensor_data is not None:
-        _send_sensor_data_to_influxdb(sensor_data)
+    try:
+        sensor_data = _parse_mqtt_message(msg.topic, msg.payload.decode('utf-8'))
+    except Exception as e:
+        print("Error with parse message:")
+        print(e)
+    if not (sensor_data is None):
+        try:
+            _send_sensor_data_to_influxdb(sensor_data)
+        except Exception as e:
+            print("Error with write to DB:")
+            print(e)
 
 
 def _parse_mqtt_message(topic, payload):
-    match = re.match(MQTT_REGEX, topic)
-    if match:
-        location = match.group(1)
-        measurement = match.group(2)
-        if measurement == 'status':
-            return None
-        return SensorData(location, measurement, float(payload))
-    else:
-        return None
+    global nodeinfoDB
+    msg = json.loads(payload)
+    print("Message:")
+    print(msg)
+    if msg['type'] == 'nodeinfo':
+        update_nodeinfoDB(msg)
+    return msg
+
+
+def add_optional_fields(orig, to, names):
+    for name in names:
+        if name in orig:
+            if name == 'snr':
+                if 200 > orig[name] > -200:
+                    to[name] = float(orig[name])
+            elif name == 'rssi':
+                if 200 > orig[name] > -200:
+                    to[name] = orig[name]
+            else:
+                to[name] = orig[name]
 
 
 def _send_sensor_data_to_influxdb(sensor_data):
-    json_body = [
-        {
-            'measurement': sensor_data.measurement,
-            'tags': {
-                'location': sensor_data.location
-            },
-            'fields': {
-                'value': sensor_data.value
-            }
-        }
-    ]
-    influxdb_client.write_points(json_body)
+    json_body = []
+    message_cnt = 1
+    if sensor_data['type'] == 'neighborinfo':
+        message_cnt = max(1, len(sensor_data['payload']['neighbors']))
+    for m_ind in range(message_cnt):
+        json_message = {'measurement': 'meshtastic',
+                        'tags': {},
+                        'fields': {}}
+
+        add_optional_fields(sensor_data, json_message['tags'], ['type', 'channel', 'from', 'sender', 'timestamp', 'to'])
+        add_optional_fields(sensor_data, json_message['fields'], ['hops_away', 'id', 'rssi', 'snr'])
+
+        # Query the database directly for node info
+        node_info_query = session.query(NodeInfo).filter_by(address=sensor_data['from']).first()
+        if node_info_query:
+            json_message['tags']['hardware'] = node_info_query.hardware
+            json_message['tags']['longname'] = node_info_query.longname
+            json_message['tags']['shortname'] = node_info_query.shortname
+        else:
+            continue
+        if 'payload' in sensor_data:
+            payload = copy.deepcopy(sensor_data['payload'])
+            if 'id' in payload:
+                del (payload['id'])
+            if sensor_data['type'] == 'neighborinfo':
+                if len(payload['neighbors']) > m_ind:
+                    payload['neighbor_node_id'] = payload['neighbors'][m_ind]['node_id']
+                    payload['neighbor_snr'] = payload['neighbors'][m_ind]['snr']
+                del (payload['neighbors'])
+            json_message['fields'].update(payload)
+
+        json_body.append(json_message)
+    if len(json_body) > 0:
+        try:
+            influxdb_client.write_points(json_body)
+        except Exception as e:
+            print("Error with influxdb:")
+            print(e)
+    print("written points:")
+    print(json_body)
+
+def update_nodeinfoDB(message):
+    nodeinfo_entry = NodeInfo(address=message['from'], **message['payload'])
+    session.merge(nodeinfo_entry)
+    session.commit()
 
 
 def _init_influxdb_database():
@@ -84,19 +164,35 @@ def _init_influxdb_database():
     influxdb_client.switch_database(INFLUXDB_DATABASE)
 
 
+@app.route('/webhook', methods=['POST'])
+def grafana_webhook():
+    data = {
+        'from': 203966156,
+        'type': 'sendtext',
+        'payload': request.json['title'] + ": " + request.json['message']
+    }
+    print("Received Grafana Alert:", data)
+    mqtt_client.publish(MQTT_TOPIC_NOTIFICATION, json.dumps(data))
+    return jsonify(success=True), 200
+
+
+mqtt_client = mqtt.Client(MQTT_CLIENT_ID)
+
+
 def main():
+    global mqtt_client
     time.sleep(10)
 
     print('Connecting to the database ' + INFLUXDB_DATABASE)
     _init_influxdb_database()
 
-    mqtt_client = mqtt.Client(MQTT_CLIENT_ID)
     mqtt_client.username_pw_set(MQTT_USER, MQTT_PASSWORD)
     mqtt_client.on_connect = on_connect
     mqtt_client.on_message = on_message
 
     mqtt_client.connect(MQTT_ADDRESS, 1883)
-    mqtt_client.loop_forever()
+    mqtt_client.loop_start()
+    app.run(host='0.0.0.0', port=5000)  # Listen for Grafana webhook on port 5000
 
 
 if __name__ == '__main__':
---
+++ 03-meshtastic/meshtastic_bridge.py
@@ -0,0 +1,114 @@
+"""Simple program to demo how to use meshtastic library.
+   To run: python examples/pub_sub_example2.py
+"""
+
+import sys
+import time
+
+from pubsub import pub
+
+import meshtastic
+import meshtastic.serial_interface
+import meshtastic.mesh_pb2
+import meshtastic.portnums_pb2
+
+import paho.mqtt.client as mqtt
+
+import json
+
+
+MQTT_ADDRESS = 'mosquitto'
+#MQTT_ADDRESS = 'localhost'
+MQTT_USER = 'mqttuser'
+MQTT_PASSWORD = 'mqttpassword'
+MQTT_CLIENT_ID = 'MeshtasticMQTTBridge'
+
+MQTT_TOPIC_NOTIFICATION = 'msh/ZS24/2/json/mqtt/'
+
+mqtt_client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, MQTT_CLIENT_ID)
+mqtt_channel = 0
+
+iface = meshtastic.serial_interface.SerialInterface(debugOut=sys.stdout)
+
+def onMestasticReceive(proxymessage, interface):  # pylint: disable=unused-argument
+    """called when a packet arrives"""
+    print(f"Received: {proxymessage}")
+    mqtt_client.publish(proxymessage.topic, proxymessage.text if hasattr(proxymessage, 'text') else proxymessage.data)
+
+def onMQTTMessage(client, userdata, msg):
+    print("Recived MQTT-Message: ")
+    print(msg.topic + ' ' + str(msg.payload))
+
+    msg = json.loads(msg.payload)
+    iface.sendText(msg['payload'][:200], channelIndex=mqtt_channel)
+
+    # m = meshtastic.mesh_pb2.MqttClientProxyMessage(topic=msg.topic, data=msg.payload, retained=msg.retain)
+    # if msg.payload['type'] == 'sendtext':
+    #p = meshtastic.mesh_pb2.MeshPacket()
+    #p.channel = mqtt_channel
+
+    #toRadio = meshtastic.mesh_pb2.ToRadio()
+    #toRadio.packet.mqttClientProxyMessage.CopyFrom(p)
+    #iface._sendToRadio(toRadio)
+    #iface.sendData(data=p, channelIndex=mqtt_channel)
+
+
+
+def onMQTTConnect(client, userdata, flags, reason_code, properties):
+    """ The callback for when the client receives a CONNACK response from the server."""
+    print('Connected with result code ' + str(reason_code))
+    client.subscribe(MQTT_TOPIC_NOTIFICATION)
+
+def getMqttChannel(iface):
+    for c in iface.localNode.channels or []:
+        if c.settings and c.settings.name.lower() == "mqtt":
+            return c.index
+    return 0
+
+def readConfig():
+    if iface.nodesByNum:
+        #logging.debug(f"self.nodes:{self.nodes}")
+        for node in iface.nodesByNum.values():
+            objDesc = node.localConfig.DESCRIPTOR
+            config_type = objDesc.fields_by_name.get('lora')
+            node.get(config_type)
+
+def onMestasticDisconnect(interface):
+    print("Reconnect to serial device")
+    interface.close()
+    time.sleep(0.1)
+    interface.connect()
+
+    interface.waitForConfig()
+    print("Reconnected")
+
+
+def main():
+    global mqtt_client, mqtt_channel, iface
+    #time.sleep(10)
+
+    mqtt_client.username_pw_set(MQTT_USER, MQTT_PASSWORD)
+    mqtt_client.on_connect = onMQTTConnect
+    mqtt_client.on_message = onMQTTMessage
+
+    mqtt_client.connect(MQTT_ADDRESS, 1883)
+    mqtt_client.loop_start()
+
+    pub.subscribe(onMestasticReceive, "meshtastic.mqttclientproxymessage")
+    pub.subscribe(onMestasticDisconnect, "meshtastic.connection.lost")
+
+
+    try:
+        mqtt_channel = iface.localNode.getChannelByName('mqtt').index
+        #readConfig()
+        while True:
+            time.sleep(1000)
+        iface.close()
+    except Exception as ex:
+        print(f"Error: Could not connect to {sys.argv[1]} {ex}")
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    print('Meshtastic to MQTT bridge')
+    main()
